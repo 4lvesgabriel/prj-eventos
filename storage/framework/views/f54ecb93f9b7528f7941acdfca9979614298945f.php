badaa9a6b0d5c5f5e388c408bc06a002dfcb6029
Gabriel Alves Gabriel <101440339+4lvesgabriel@users.noreply.github.com>
2024-01-15 14:02:25 -0300

    Add files via upload

---
+++ storage/framework/views/f54ecb93f9b7528f7941acdfca9979614298945f.php
@@ -0,0 +1,6 @@
+<?php $__env->startSection('title', "Dashboard"); ?>
+<?php $__env->startSection("content"); ?>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\wamp64\www\prjeventos\resources\views/dashboard.blade.php ENDPATH**/ ?>
